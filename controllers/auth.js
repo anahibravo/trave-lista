058b4a79e7b96faaaabb7f01e31bd6eac6b27a5b
anahibravo <anahi.bravo@protonmail.com>
2023-01-14 22:39:47 -0800

    added profile.ejs and it routed to it after clicking login

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,21 +31,25 @@ const User = require('../models/User')
       req.logIn(user, (err) => {
         if (err) { return next(err) }
         req.flash('success', { msg: 'Success! You are logged in.' })
-        res.redirect(req.session.returnTo || '/todos')
+        res.redirect(req.session.returnTo || '/profile')
       })
     })(req, res, next)
   }
   
-exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
-    })
-  }
+//new logout method
+  exports.logout = (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      console.log("User has logged out.");
+      req.session.destroy((err) => {
+        if (err) console.log("Error : Failed to destroy the session during logout.", err);
+        req.user = null;
+        res.redirect("/");
+      });
+    });
+  };
   
   exports.getSignup = (req, res) => {
     if (req.user) {
